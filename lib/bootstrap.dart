b6b9e641190dcab5500d286a601845ca502b3332
arl <arl@anadeainc.com>
2023-08-13 12:56:41 +0300

    feat(i18n): setup i18n

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/widgets.dart';
 
 import 'injection/injection.dart';
+import 'presentation/I18n/i18n.dart';
 
 Future<void> bootstrap(Widget Function() callback) async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
+  LocaleSettings.useDeviceLocale();
+
   configureDependencies();
 
-  runApp(callback());
+  runApp(TranslationProvider(child: callback()));
 }
--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'I18n/i18n.dart';
 import 'routes/routes.dart';
 import '../injection/injection.dart';
 
@@ -9,6 +11,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      locale: TranslationProvider.of(context).flutterLocale,
+      supportedLocales: AppLocaleUtils.supportedLocales,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
       routerConfig: sl<AppRouter>().config(),
     );
   }
